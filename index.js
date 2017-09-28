054712913f8f8abbafaacb434a6f5548085b60d2
rafosorio <rafosorio@gmail.com>
2017-09-28 15:16:29 -0300

    ok
    ok

---
+++ index.js
@@ -0,0 +1,87 @@
+"use strict";
+// --------------------------------------------------------------------------
+// Require statements
+// --------------------------------------------------------------------------
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var requestjs = require("request-json");
+
+var APP_ID = "a94753f5-???";
+var APP_SECRET = "4UZLpTis-??";
+var SPACE_ID = "5819???";
+
+// --------------------------------------------------------------------------
+// Setup global variables
+// --------------------------------------------------------------------------
+
+// Workspace API Setup - fixed stuff
+const WWS_URL = "https://api.watsonwork.ibm.com";
+const AUTHORIZATION_API = "/oauth/token";
+const OAUTH_ENDPOINT = "/oauth/authorize";
+
+// --------------------------------------------------------------------------
+// Setup the express server
+// --------------------------------------------------------------------------
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// --------------------------------------------------------------------------
+// Express Server runtime
+// --------------------------------------------------------------------------
+// Start our server !
+app.listen(process.env.PORT || 3000, function() {
+    console.log("INFO: app is listening on port %s", (process.env.PORT || 3000));
+});
+
+
+
+// --------------------------------------------------------------------------
+// REST API test : listen for GET requests on /inspiration
+app.get("/inspiration", function(req, res) {
+    console.log(req.body);
+    res.send("Are you looking for inspiration?");
+});
+
+//--------------------------------------------------------------------------
+//Get an authentication token
+function getJWTToken(userid, password, callback) {
+    // Build request options for authentication.
+    const authenticationOptions = {
+        "method": "POST",
+        "url": `${WWS_URL}${AUTHORIZATION_API}`,
+        "auth": {
+            "user": userid,
+            "pass": password
+        },
+        "form": {
+            "grant_type": "client_credentials"
+        }
+    };
+
+    // Get the JWT Token
+    request(authenticationOptions, function(err, response, authenticationBody) {
+
+        // If successful authentication, a 200 response code is returned
+        if (response.statusCode !== 200) {
+            // if our app can't authenticate then it must have been
+            // disabled. Just return
+            console.log("ERROR: App can't authenticate");
+            callback(null);
+        }
+        const accessToken = JSON.parse(authenticationBody).access_token;
+        callback(accessToken);
+    });
+}
+
+//--------------------------------------------------------------------------
+//Post a message to a space
+function postMessageToSpace(spaceId, accessToken, textMsg, callback) {
+
+
+}
